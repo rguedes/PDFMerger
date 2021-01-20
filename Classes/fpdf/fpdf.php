534e000591e7eeed5f6a0ad60aa1e59cea54afac
Marcel Schindler <marcel.schindler@lobo-tueren.de>
2021-01-20 09:04:00 +0100

    Magic Quotes Test

--- Classes/fpdf/fpdf.php
+++ Classes/fpdf/fpdf.php
@@ -1040,7 +1040,7 @@ protected function _dochecks()
 	if(ini_get('mbstring.func_overload') & 2)
 		$this->Error('mbstring overloading must be disabled');
 	// Ensure runtime magic quotes are disabled
-	if(get_magic_quotes_runtime())
+	if(function_exists('get_magic_quotes_runtime') && @get_magic_quotes_runtime())
 		@set_magic_quotes_runtime(0);
 }
 
