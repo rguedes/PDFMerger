670b1a65a671cb3317b8bb109a811ceffc4d75c2
AndriiHrynchuk <andrii@amplerbikes.com>
2021-01-27 14:57:06 +0200

    added laravel example to readme

--- README.md
+++ README.md
@@ -22,3 +22,15 @@ $pdf->merge('file', 'samplepdfs/TEST2.pdf');
     
 // REPLACE 'file' WITH 'browser', 'download', 'string', or 'file' for output options
 ```
+
+### Laravel example\
+```php
+
+use \PDFMerger;
+
+$pdf = new PDFMerger();
+
+$pdf->addPDF(storage_path('app/public/' . 'samplepdfs/one.pdf'), '1, 3, 4')
+	->addPDF(storage_path('app/public/' . 'samplepdfs/two.pdf'), '1-2')
+	->addPDF(storage_path('app/public/' . 'samplepdfs/three.pdf'), 'all')
+	->merge('file', storage_path("app/public/merged_pdf.pdf"));
--- sample.php
+++ sample.php
@@ -6,7 +6,7 @@ $pdf = new PDFMerger();
 $pdf->addPDF(storage_path('app/public/' . 'samplepdfs/one.pdf'), '1, 3, 4')
 	->addPDF(storage_path('app/public/' . 'samplepdfs/two.pdf'), '1-2')
 	->addPDF(storage_path('app/public/' . 'samplepdfs/three.pdf'), 'all')
-	->merge('file', storage_path("app/public/merged_invoice.pdf"));
+	->merge('file', storage_path("app/public/merged_pdf.pdf"));
 	
 	//REPLACE 'file' WITH 'browser', 'download', 'string', or 'file' for output options
 	//You do not need to give a file path for browser, string, or download - just the name.
